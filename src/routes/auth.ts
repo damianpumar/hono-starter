24039d4d692d1f749749100b186c3b9405a0aa65
Damián Pumar <damianpumar@gmail.com>
2024-03-04 09:23:24 +0100

    ✨ Improve sign in

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,39 +1,59 @@
 import { Hono } from "hono";
 import { deleteCookie, setSignedCookie } from "hono/cookie";
-import { jwt, sign } from "hono/jwt";
+import { sign } from "hono/jwt";
 import { authMiddleware } from "../middlewares";
 import config from "../config";
+
+type SignInBody = {
+  email: string;
+  password: string;
+  strategy: "cookie" | "bearer";
+};
 
 const routes = new Hono<HonoVariables>();
 const name = "auth";
 
 routes.post("/sign-in", async (c) => {
+  const { email, password, strategy } = await c.req.json<SignInBody>();
+
   const user = {
     id: 1,
   };
 
-  await setSignedCookie(
-    c,
-    config.auth.cookie.key,
-    btoa(JSON.stringify(user)),
-    config.auth.secret,
-    {
-      httpOnly: true,
-      expires: new Date(
-        Date.now() + 1000 * 60 * 60 * 24 * config.auth.cookie.expirationDays
-      ),
-      sameSite: "None",
-      domain: config.auth.cookie.domain,
-      secure: true,
-    }
-  );
+  if (strategy === "bearer") {
+    const token = await sign(user, config.auth.secret, "HS512");
 
-  const token = await sign(user, config.auth.secret, "HS512");
+    return c.json({
+      token,
+      strategy,
+      message: "Sign-in successful",
+    });
+  }
 
-  return c.json({
-    token,
-    message: "Sign-in successful",
-  });
+  if (strategy === "cookie") {
+    await setSignedCookie(
+      c,
+      config.auth.cookie.key,
+      btoa(JSON.stringify(user)),
+      config.auth.secret,
+      {
+        httpOnly: true,
+        expires: new Date(
+          Date.now() + 1000 * 60 * 60 * 24 * config.auth.cookie.expirationDays
+        ),
+        sameSite: "None",
+        domain: config.auth.cookie.domain,
+        secure: true,
+      }
+    );
+
+    return c.json({
+      strategy,
+      message: "Sign-in successful",
+    });
+  }
+
+  return c.json({ error: "Invalid strategy to sign-in" }, 400);
 });
 
 routes.get("/me", authMiddleware(), (c) => {
