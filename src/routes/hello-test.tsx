9ebe29f66bc90cdf63179345c607b409723e2a6f
Damián Pumar <damianpumar@gmail.com>
2024-03-08 00:37:00 +0100

    ✨ Try vue as ssr

--- src/routes/hello-test.tsx
+++ src/routes/hello-test.tsx
@@ -16,54 +16,74 @@ routes.post("/data", zValidator("json", schema), (c) => {
   return c.json({ ok: true, body });
 });
 
+const Header =
+  () => html` <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script>
+      const { createApp, ref } = Vue;
+
+      const counter = ref(0);
+    </script>`;
+
 const Counter = () => html`
-  <button @click="counter++">Clicked {{ counter }} times</button>
+  <div id="counter">
+    <button @click="increment">Clicked {{ counter }} times</button>
+  </div>
+
+  <script>
+    createApp({
+      setup() {
+        const increment = () => {
+          counter.value++;
+        };
+
+        return { counter, increment };
+      },
+    }).mount("#counter");
+  </script>
+`;
+
+const DataLoader = () => html`
+  <div id="data-loader" style="margin-top: 20px">
+    <button @click="loadData">Load data</button>
+    <p>{{ data }}</p>
+  </div>
+
+  <script>
+    createApp({
+      setup() {
+        const data = ref("No data");
+
+        const loadData = () => {
+          fetch("/ssr/data", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ hello: "world", counter: counter.value }),
+          })
+            .then((res) => res.json())
+            .then((res) => {
+              data.value = JSON.stringify(res);
+            });
+        };
+
+        return { data, loadData };
+      },
+    }).mount("#data-loader");
+  </script>
 `;
 
 routes.get("/", (c) => {
   return c.html(
     <html>
       <body>
-        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+        <Header />
 
-        <div id="app">
-          <h2>Backend rendering!</h2>
+        <h2>Backend rendering!</h2>
 
-          <Counter />
+        <Counter />
 
-          {raw(`<button @click="loadData">Load data</button>`)}
-
-          {raw(`<p>{{ data }}</p>`)}
-
-          <script>
-            {raw(`
-              const { createApp, ref } = Vue;
-
-              createApp({
-                setup() {
-                  const counter = ref(0);
-                  const data = ref("No data");
-
-                  const loadData = () => {
-                    fetch("/ssr/data", {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify({ hello: "world", counter: counter.value }),
-                    })
-                      .then((res) => res.json())
-                      .then((res) => {
-                        data.value = JSON.stringify(res);
-                      });
-                  }
-
-                  return { counter, data, loadData };
-                },
-              }).mount("#app");
-            `)}
-          </script>
-        </div>
+        <DataLoader />
       </body>
     </html>
   );
